3ad399901c37cb700ce3a33f19e935aec42d1fd1
Mohammad shadaab <mdshadaab.dev@gmail.com>
2021-12-25 14:19:30 +0530

    Added Validation Response using spring Validation

--- src/main/java/com/springboot/blog/payload/PostDto.java
+++ src/main/java/com/springboot/blog/payload/PostDto.java
@@ -3,14 +3,20 @@ package com.springboot.blog.payload;
 import com.springboot.blog.entity.Comment;
 import lombok.Data;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 import java.util.Set;
 
 @Data
 public class PostDto {
     private Long id;
+    @NotEmpty @Size(min = 3,max=20 ,message = "Post Title should have Minimum 3 and Maximum 20 characters")
     private String title;
+    @NotEmpty @Size(min = 10, message = "Post Description should have Minimum 10 characters")
     private String description;
+    @NotEmpty
     private String content;
+
     private Set<Comment> comments;
 
 }
